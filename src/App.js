e2679719e4b16b8a8c7d559ce0fc848e382f1f0c
Babak <babak.jahedmanesh@thenavpeople.co.uk>
2022-02-12 16:20:36 +0000

    load/tree shaking test on build

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      </header>
+    </div>
+  );
+}
+
+export function App2() {
+  return (
+    <div className="App2">
+      <header className="App-header2">
+        <p>
+          Edit <code>src/App2.js</code> and save to reload.
+        </p>
       </header>
     </div>
   );
